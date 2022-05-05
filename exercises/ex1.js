ca8beb85a95f8923d3a3ed1c5c7146f961763e85
Atsuya Tanaka <tenmusu007@gmail.com>
2022-05-05 09:00:07 -0700

    atsu

--- exercises/ex1.js
+++ exercises/ex1.js
@@ -8,9 +8,9 @@ Hint: Use the %(modulo) operator to determine is a number is even or odd.
 
 const number = 2; // Change your number to test your if statement
 
-if (/* your conditional goes here */) {
-
+if (number % 2) {
+    console.log("number is even")
 } else {
-
-}
+    console.log("number is odd")
+};
 
--- exercises/ex2.js
+++ exercises/ex2.js
@@ -9,3 +9,15 @@ Write a series of conditional statements that:
 - Prints "this is a large group" if musicians is greater than 4
 
 */
+let group = 1;
+if(group === 1){
+    console.log('solo')
+}else if (group ===2){
+    console.log("duet")
+}else if (group === 3){
+    console.log("trio")
+}else if (group === 4){
+    console.log("quartet")
+}else if(group > 4){
+    console.log("this is large group")
+}
