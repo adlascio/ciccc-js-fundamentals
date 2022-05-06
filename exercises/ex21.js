5b0937cdb1ab1396e1a6793e823337970d089f06
Atsuya Tanaka <tenmusu007@gmail.com>
2022-05-06 13:21:40 -0700

    atsu

--- exercises/ex21.js
+++ exercises/ex21.js
@@ -1,8 +1,28 @@
 /*
 Starting with this array of prices, change the prices of the 1st, 3rd, and 7th elements in the array.
 
-var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
 TIP: The 1st element of any array has an index of 0.
 
 Afterwards, print out the prices array to the console.
 */
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+prices.splice(0, 1, 3)
+prices.splice(1, 1, 3)
+prices.splice(7, 1, 3)
+
+console.log(prices)
+// function changePrice(arrayData){
+//     let newArray = arrayData
+//     newArray.push(3.00)
+// }
+// changePrice(change1)
+
+// 配列型データ定義
+// let fruits = ["apple", "melon", "orange"];
+// // 引数に配列型をセットすることを前提とした関数定義
+// function testFn( arrayData ){
+//   console.log("本日のフルーツ一覧");
+//   console.log(arrayData)
+// }
+// // 関数の実行
+// testFn( fruits );
--- exercises/ex23.js
+++ exercises/ex23.js
@@ -1,8 +1,25 @@
 /*
-const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
 
 Write a loop under each comment in the file.
 Under the first, write a while loop that prints out each item of ingredients.
 Under the second, write a for loop that does the same thing.
 And under the third, write any loop, while or for that prints out each item of ingredients but backwards (that is, the first should be "bananas" and the last "eggs").
 */
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+const whileLoop = () =>{
+}
+const forLoop = () =>{
+    for(let i = 0; i < ingredients.length; i++){
+        console.log(ingredients[i])
+    }
+}
+forLoop()
+
+
+const eachForingredent = function(arrayData){
+    arrayData.forEach(function (item){
+        console.log((item))
+    });
+}
+eachForingredent(ingredients)
+
